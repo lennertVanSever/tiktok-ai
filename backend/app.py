e0af9de4098620562627ce76c37cc05904d2902d
lennert <lennert@lennerts-MacBook-Air.local>
2023-11-07 23:21:11 +0100

    elect most relevant keyword

--- backend/app.py
+++ backend/app.py
@@ -40,7 +40,39 @@ def get_videos():
     return jsonify(videos)
 
 
+def predict_watch_time(keyword_watch_data, similarity_matrix):
+    # Create a dictionary to hold the predicted watch times
+    predicted_watch_times = {}
+
+    # Iterate over each tag for which we have watch data
+    for tag, watch_time in keyword_watch_data.items():
+        # Iterate over each related tag in our similarity matrix
+        for related_tag, similarity_score in similarity_matrix[tag].items():
+            # If we don't already have watch data for the related tag
+            if related_tag not in keyword_watch_data:
+                # Initialize the related tag in our predicted_watch_times if it's not there yet
+                if related_tag not in predicted_watch_times:
+                    predicted_watch_times[related_tag] = {
+                        'total_score': 0, 'weighted_score': 0}
+
+                # Aggregate the weighted score and total similarity score for the related tag
+                predicted_watch_times[related_tag]['weighted_score'] += watch_time * \
+                    similarity_score
+                predicted_watch_times[related_tag]['total_score'] += similarity_score
+
+    # Calculate the final predicted watch time for each related tag by normalizing the weighted score by the total score
+    for tag, scores in predicted_watch_times.items():
+        if scores['total_score'] > 0:  # Avoid division by zero
+            predicted_watch_times[tag] = scores['weighted_score'] / \
+                scores['total_score']
+        else:
+            predicted_watch_times[tag] = 0
+
+    return predicted_watch_times
+
 # Function to call the Datamuse API for each tag and build a similarity matrix
+
+
 def build_similarity_matrix(tags):
     # Initialize the similarity matrix as a nested dictionary
     # where each tag will contain a dictionary of similar words with their scores
@@ -67,20 +99,42 @@ def build_similarity_matrix(tags):
 
 # Example usage within your Flask route:
 
+# Inside your handle_keyword_data route
+
+
+def find_most_relevant_keyword(keyword_watch_data, similarity_matrix):
+    # Dictionary to hold the cumulative score of each keyword
+    keyword_scores = {}
+
+    # Calculate the weighted score for each similar word based on the watch time
+    for keyword, watch_time in keyword_watch_data.items():
+        similar_words = similarity_matrix.get(keyword, {})
+        for similar_word, score in similar_words.items():
+            if similar_word not in keyword_watch_data:  # Avoid keywords already watched
+                keyword_scores[similar_word] = keyword_scores.get(
+                    similar_word, 0) + (score * watch_time)
+
+    # Find the keyword with the highest cumulative score
+    most_relevant_keyword = max(
+        keyword_scores, key=keyword_scores.get) if keyword_scores else None
+    return most_relevant_keyword, keyword_scores[most_relevant_keyword] if most_relevant_keyword else 0
+
 
 @app.route('/endpoint-to-handle-keyword-data', methods=['POST'])
 def handle_keyword_data():
     keyword_watch_data = request.json
-    print(keyword_watch_data)
     tags = list(keyword_watch_data.keys())
-    # Build the similarity matrix for the tags received from the frontend
     similarity_matrix = build_similarity_matrix(tags)
 
-    # Now you have a similarity matrix that you can use for collaborative filtering
-    # The rest of your collaborative filtering logic would go here
+    # Find the most relevant keyword
+    most_relevant_keyword, score = find_most_relevant_keyword(
+        keyword_watch_data, similarity_matrix)
 
-    # For now, just return the similarity matrix for demonstration
-    return jsonify(similarity_matrix), 200
+    # Return the most relevant keyword and its score
+    return jsonify({
+        'most_relevant_keyword': most_relevant_keyword,
+        'score': score
+    }), 200
 
 
 @app.route('/')
