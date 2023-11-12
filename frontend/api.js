b63a9a9d50f42a0490494dbdbba74a125f21df83
Lennert <lennert_vansever@hotmail.be>
2023-11-12 19:46:56 +0100

    improve logging

--- frontend/api.js
+++ frontend/api.js
@@ -3,11 +3,31 @@ import { initVideosAndStats } from "./videoStats.js";
 
 export let keywordWatchTimes = {}; // Keep track of keyword watch times
 export let discardedKeywordWatchTimes = {}; // Keep track of keyword watch times
-const stats = []
+const stats = [];
+
+
+function formatTable(data) {
+    let maxKeywordLength = Math.max(...Object.keys(data).map(keyword => keyword.length), 'Keyword'.length);
+    let header = `Keyword${' '.repeat(maxKeywordLength - 'Keyword'.length)} | Watchtime`;
+    let separator = '-'.repeat(header.length);
+
+    let table = [header, separator];
+
+    for (const [keyword, watchTime] of Object.entries(data)) {
+        let paddedKeyword = keyword + ' '.repeat(maxKeywordLength - keyword.length);
+        table.push(`${paddedKeyword} | ${(Math.round(watchTime * 100) / 100).toFixed(2)}s`);
+    }
+
+    return table.join('\n');
+}
+
 
 export function sendKeywordWatchTimes() {
-    // console.log('Sending keywordWatchTimes:', keywordWatchTimes);
-    const tempKeywordWatchTimes = { ...keywordWatchTimes }
+
+    console.log(`Information send to server: \n${formatTable(keywordWatchTimes)}`);
+
+
+    const tempKeywordWatchTimes = { ...keywordWatchTimes };
     fetch('/endpoint-to-handle-keyword-data', {
         method: 'POST',
         headers: {
@@ -21,15 +41,16 @@ export function sendKeywordWatchTimes() {
                 keywordWatchTimes: tempKeywordWatchTimes,
                 mostRelevantKeyword: data.most_relevant_keyword,
                 similarityMatrix: data.similarity_matrix,
-            })
+            });
             Object.keys(keywordWatchTimes).forEach((keyword) => {
                 discardedKeywordWatchTimes[keyword] = keywordWatchTimes[keyword];
                 delete keywordWatchTimes[keyword];
             });
             if (data.videos && data.videos.length > 0) {
+                console.log('Information received from server', data)
                 initVideosAndStats(data.videos);
             }
-            renderStats(stats)
+            renderStats(stats);
         })
         .catch(error => {
             console.error('Error:', error);
